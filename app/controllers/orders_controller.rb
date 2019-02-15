978e041392a07071ceb1c762c1597b6f28e6d183
sureshLuxire <suresh@luxire.com>
2019-02-15 17:49:46 +0530

    commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -51,7 +51,7 @@ class OrdersController < ShopifyApp::AuthenticatedController
     if params[:id].present?
       #updated_tags = cur_tag.split(',').map(&:strip)
       #@order.tags = updated_tags.uniq.join(',')
-      @order.tags.push(cur_tag)
+      @order.tags.(first) = cur_tag
       @order.save
       respond_to do |format|
         format.html { redirect_to orders_url, notice: 'Order was successfully updated..' }
