6e1f00d8c14932461518e349e6a7816f457b0bad
sureshLuxire <suresh@luxire.com>
2019-02-15 14:22:46 +0530

    commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -48,7 +48,7 @@ class OrdersController < ShopifyApp::AuthenticatedController
     @order = ShopifyAPI::Order.find(params[:id])
     arrays = params[:tags]
     if params[:id].present?
-      @order.tags = arrays
+      @order.tags = @order.tags + params[:tags]
       @order.save
       respond_to do |format|
         format.html { redirect_to orders_url, notice: 'Order was successfully updated..' }
