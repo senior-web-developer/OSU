9ba7a7a5fad268caab13d4843a34e777b6e025e9
sureshLuxire <suresh@luxire.com>
2019-02-20 14:37:27 +0530

    files update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -71,7 +71,7 @@ class OrdersController < ShopifyApp::AuthenticatedController
         cur_tag = [cur_tag] + [new_tag]
         @order.tags = cur_tag.join(', ') 
         else
-          cur_tag.delete_at(0)
+          cur_tag.delete_at("STATUS:Unfulfilled")
         end
         
 
