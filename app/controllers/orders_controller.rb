be9e924ec5f15e7cb5c6a631522c1b0fd799b43b
sureshLuxire <suresh@luxire.com>
2019-02-21 20:30:42 +0530

    files update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -62,7 +62,7 @@ class OrdersController < ShopifyApp::AuthenticatedController
       cur_tags = @order.tags.split(", ")
       new_tags = params[:tags]
 
-      for i in 0..cur_tags.length+1         
+      for i in 0..cur_tags.length        
              if cur_tags[i][0, 7] == "STATUS:"
                  cur_tags[i] = new_tags
                  @order.tags = [cur_tags].join(', ')
