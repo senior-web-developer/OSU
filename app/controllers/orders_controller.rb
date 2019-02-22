883f2420c6d9407f2a46a58cb2125e1c9f303a3b
sureshLuxire <suresh@luxire.com>
2019-02-22 11:57:31 +0530

    files update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -59,20 +59,18 @@ class OrdersController < ShopifyApp::AuthenticatedController
     
     if params[:id].present?
 
-      [cur_tags] = @order.tags.split(", ")
+      cur_tags = @order.tags.split(", ")
       new_tags = params[:tags]        
-
     
       for i in 0..(cur_tags.length)-1         
              if cur_tags[i][0, 6] == "STATUS"
                  cur_tags[i] = new_tags
-                 @order.tags = [cur_tags].join(", ")
+                 @order.tags = cur_tags
                 else       
                  cur_tags = [cur_tags] + [new_tags]
-                 @order.tags = [cur_tags].join(", ")
+                 @order.tags = cur_tags
              end        
       end
-  
 
     @order.save
       respond_to do |format|
