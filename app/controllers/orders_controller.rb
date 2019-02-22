5871365a8888f839d2f781f27363eb4b802386fc
sureshLuxire <suresh@luxire.com>
2019-02-22 17:46:48 +0530

    files update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -68,9 +68,11 @@ class OrdersController < ShopifyApp::AuthenticatedController
 
         if cur_tags[i][0, 7] == "Status:"
           cur_tags[i] = [new_tags]
-          @order.tags = cur_tags.join(", ")                     
+          @order.tags = cur_tags.join(", ")
+        else
+          cur_tags = [cur_tags] + [new_tags]
+          @order.tags = cur_tags.join(", ")              
         end
-
       end
 
     @order.save
