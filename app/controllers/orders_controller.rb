aa2db39e0c25826c5706b04f0fa6e2f5fe0d3b4c
sureshLuxire <suresh@luxire.com>
2019-02-22 20:14:46 +0530

    files update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -77,7 +77,7 @@ class OrdersController < ShopifyApp::AuthenticatedController
 
       for i in 0..new_tags.length-1
         cur_tags = new_tags[i]
-        @order.tags = cur_tags.join(",") 
+        @order.tags = cur_tags 
       end   #End For
 
     @order.save
